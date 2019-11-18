849b67ab3369410df7040f8d2bd2aad32d9fad4a
Amine L'ch <aminelch95@gmail.com>
2019-11-18 22:43:20 +0100

    Ajout de classTestMetier

---
+++ Atelier0/src/metier/TestMetier.java
@@ -0,0 +1,14 @@
+package metier;
+
+public class TestMetier {
+
+	public static void main(String[] args) {
+		SommeMetierImpl metier = new SommeMetierImpl();
+		double v1=3;
+		double v2=6;
+		double s=metier.calculerSomme(v1, v2);
+		System.out.println("La somme est :"+s);
+		}
+	
+	
+}
